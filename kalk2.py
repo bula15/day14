b9b6c8655a903a5a063fab67284302e175409390
baked <rollerboy15>
2017-03-16 19:26:34 +0100

    b

---
+++ kalk2.py
@@ -0,0 +1,12 @@
+import sys
+def divide(a,b):
+    return a/b
+def main():
+    a= int(sys.argv[1])
+    b= int(sys.argv[2])
+
+    wynik = divide(a, b)
+    print(wynik)
+
+if __name__=='__main__'
+    main()
